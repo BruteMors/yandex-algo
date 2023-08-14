ec9f4ea31a2012a813ca502fad0b630c929d3cf5
Anton <anton.git.dev@gmail.com>
2023-08-14 18:38:01 +0300

    Solve bubble sort task

---
+++ task1/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	numOfElem := 0
+
+	in := bufio.NewReader(os.Stdin)
+	out := bufio.NewWriter(os.Stdout)
+	defer out.Flush()
+
+	_, err := fmt.Fscanln(in, &numOfElem)
+	if err != nil {
+		return
+	}
+
+	arr := make([]int, numOfElem)
+	for i := 0; i < numOfElem; i++ {
+		var num int
+		fmt.Fscan(in, &num)
+		arr[i] = num
+	}
+
+	for i := 0; i < len(arr); i++ {
+		for j := 0; j < len(arr)-1; j++ {
+			if arr[j] > arr[j+1] {
+				temp := arr[j]
+				arr[j] = arr[j+1]
+				arr[j+1] = temp
+			}
+		}
+	}
+
+	fmt.Fprintln(out, arr)
+
+}
